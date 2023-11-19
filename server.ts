7201a5889119c0a238ffcb24123c266c31ec8705
rakeshdhariwa <rakeshdhariwal61@gmail.com>
2023-11-19 03:47:52 +0000

    masterghost2002: added route to server client static files

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response } from 'express';
+import path from 'path';
 const PORT = process.env.PORT || 5000;
 const server = express();
-server.get('/', (req:Request, res:Response)=>{
-    return res.status(200).json('OK');
-});
+
+// serve the static files only for production
+server.use(express.static(path.join(__dirname, './web/dist')));
+server.get('/', (req:Request, res:Response)=>res.sendStatus(200));
+
+server.get('/health', (req:Request, res:Response)=>res.status(200).json('Server is running'));
 server.listen(PORT, ()=>console.log('Listening  to port ', PORT));
 
