f91c32614cbd44363f980eb308e02d4b8d169df1
rakeshdhariwa <rakeshdhariwal61@gmail.com>
2023-11-20 17:26:19 +0000

    masterghost2002:added shadcn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import useTheme from "./hooks/useTheme"
+import { themeType } from "../types/types.d";
 function App() {
 
+  const setTheme = useTheme();
   return (
-    <div
-      className="bg-white color-red"
+    <Card
+      className="bg-dark-bg color-white"
     >
-      NoteWoven 
-    </div>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+  <button onClick={()=>setTheme(themeType.dark)}>Dark</button>
+  <button onClick={()=>setTheme(themeType.light)}>Light</button>
+
+</Card>
+
   )
 }
 
