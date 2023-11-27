13b8bd5154c478dcb096c665d27ffcffe0edd39c
rakeshdhariwa <rakeshdhariwal61@gmail.com>
2023-11-27 08:41:58 +0000

    masterghost2002:added route for sign-in

--- controller/user/sing-in/route.ts
+++ controller/user/sing-in/route.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
-import prisma from '../../prisma';
-import ApiResponse from '../../util/Api/ApiResponse';
+import cryptr from '../../../util/cryptr';
+import prisma from '../../../prisma';
+import ApiResponse from '../../../util/Api/ApiResponse';
 import loginFields from './validation.zod';
 const POST = async (req: Request, res: Response) => {
     const loginData = req.body.loginData;
@@ -8,7 +9,7 @@ const POST = async (req: Request, res: Response) => {
         const parsedLoginFields = loginFields.safeParse(loginData);
         if(!parsedLoginFields.success)
             return res.status(400).json(new ApiResponse(200, parsedLoginFields.error.errors, 'Invalid login data'));
-        const user = await prisma.user.findFirst({
+        const data = await prisma.user.findFirst({
             where:{
                 OR:[
                     {email:parsedLoginFields.data.credential},
@@ -19,8 +20,14 @@ const POST = async (req: Request, res: Response) => {
                 admin:true
             }
         });
-        if(!user)
+        if(!data)
             return res.status(404).json(new ApiResponse(404, {}, 'User not found'));
+
+        const {password, ...user} = data;
+        const decryptedPassword = cryptr.decrypt(password);
+
+        if(decryptedPassword !== parsedLoginFields.data.password)
+            return res.status(401).json(new ApiResponse(401, {}, 'Incorrect Password'));
         return res.status(200).json(new ApiResponse(200, {user}, 'Login success'))
     } catch (error) {
         console.log('Error at controller/user/route',error);
--- routes/user/route.ts
+++ routes/user/route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import GETUSER from '../../controller/user/route';
+import GETUSER from '../../controller/user/sing-in/route';
 const router = express.Router();
 router.post('/sign-in', GETUSER);
 export default router;
