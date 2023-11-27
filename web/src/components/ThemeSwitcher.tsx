6ebd09a20416d69e3bb7bc416fba21c727f1c2b8
rakeshdhariwa <rakeshdhariwal61@gmail.com>
2023-11-27 15:56:19 +0000

    masterghost2002:fixed button inside button issue

--- web/src/components/ThemeSwitcher.tsx
+++ web/src/components/ThemeSwitcher.tsx
@@ -7,19 +7,16 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Sun, Moon } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import useTheme from "@/hooks/useTheme";
 import { themeType } from "@types";
 export default function ThemeSwitcher() {
     const setTheme = useTheme();
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button className="bg-transparent border-0" variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <DropdownMenuTrigger className="h-[1.2rem] w-[1.2rem]">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 dark:hidden" />
+                    <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 hidden dark:block" />
                     <span className="sr-only">Toggle theme</span>
-                </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent
                 className="
