a0693522008019fe117e156d99482bed1e25603f
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-01-06 08:05:25 +0000

    typo fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import PublicLayout from '@/layouts/Public.layout';
-import SignInPage from '@/pages/SignInPage';
+import SignInPage from '@/pages/SignIn.page';
 import ServerSetup from '@/pages/ServerSetup.page';
 function App() {
   return (
