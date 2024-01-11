c7477ff0fbd189a02d974178fd3bb3bc6daefb0f
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-01-11 11:37:38 +0000

    masterghost2002:added validate token in protect layout

--- web/src/layouts/Protected.layout.tsx
+++ web/src/layouts/Protected.layout.tsx
@@ -6,36 +6,42 @@ import Header from "@/components/Header";
 import Sidenavbar from "@/components/navigation/Sidenavbar";
 import Main from "@/components/Main";
 import useUserStore from "@/store/userUserStore";
+import { UserType } from "@types";
 import { createAxiosInstance } from "@/util/ApiHandler";
+// 
+const isValidating = signal(false);
 const ProtectedLayout = () => {
     // get user from store
     const user = useUserStore(state => state.user);
+    const setUser = useUserStore(state=>state.setUser);
     const accessToken = user.accessToken;
 
     // 
     const navigate = useNavigate();
 
     const validateToken = useCallback(async () => {
+        isValidating.value = true;
         const api = createAxiosInstance(accessToken);
 
         try {
-            const response = await api.get('/api/user/validate-token');
-            console.log(response);
+            const response = await api.post('/api/user/renew-token');
+            const user:UserType = response.data.data;
+            setUser(user);
+            isValidating.value = false;
         } catch (error: any) {
-            const statusCode = error.response.satus;
+            const statusCode = error.response.status;
             const errorMessage = error.response?.data?.message || 'Server Error';
-            console.log(errorMessage);
-
+            isValidating.value = false;
             toast.error(errorMessage);
-            if (statusCode === 403)
+            if (statusCode === 401)
                 navigate('/welcome');
         }
 
-    }, [accessToken, navigate, toast]);
+    }, [accessToken, navigate, toast,setUser]);
 
     useEffect(() => {
-        // validateToken();
-    }, []);
+        validateToken();
+    }, [validateToken]);
 
     return (
         <div
